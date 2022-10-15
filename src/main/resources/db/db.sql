1e1dab1e5425f10f67f9887a756fafb49ee0e39e
leonardo.berlatto <lberlatto@tempo.io>
2022-10-15 20:32:25 -0300

    Adicionando script de dados

---
+++ src/main/resources/db/db.sql
@@ -0,0 +1,74 @@
+insert into referencia_geografica (nome, latitude, longitude) values ('R31', 3,1);
+insert into referencia_geografica (nome, latitude, longitude) values ('R51', 5,1);
+insert into referencia_geografica (nome, latitude, longitude) values ('R71', 7,1);
+
+insert into referencia_geografica (nome, latitude, longitude) values ('R22', 2,2);
+insert into referencia_geografica (nome, latitude, longitude) values ('R92', 9,2 );
+insert into referencia_geografica (nome, latitude, longitude) values ('R53', 5,3);
+insert into referencia_geografica (nome, latitude, longitude) values ('R73', 7,3);
+
+insert into referencia_geografica (nome, latitude, longitude) values ('R14', 1,4);
+insert into referencia_geografica (nome, latitude, longitude) values ('R34', 3,4);
+insert into referencia_geografica (nome, latitude, longitude) values ('R84', 8,4);
+insert into referencia_geografica (nome, latitude, longitude) values ('R55', 5,5);
+
+insert into referencia_geografica (nome, latitude, longitude) values ('R26', 2,6);
+insert into referencia_geografica (nome, latitude, longitude) values ('R46', 4,6);
+insert into referencia_geografica (nome, latitude, longitude) values ('R86', 8,6);
+
+
+insert into aeroporto values ('SDU', 'Santos Dummont', 'R71');
+insert into aeroporto values ('GRU', 'Guarulhos', 'R22');
+insert into aeroporto values ('CGH', 'Congonhas', 'R53');
+insert into aeroporto values ('POA', 'Porto Alegre - Salgado Filho', 'R84');
+insert into aeroporto values ('CGB', 'Marechal Rondon', 'R26');
+insert into aeroporto values ('RBR', 'Plácido de Castro', 'R55');
+
+
+insert into aerovia values ('AV1', 1000, 'R22', 'R31');
+insert into aerovia values ('AV2', 1000, 'R31', 'R51');
+insert into aerovia values ('AV3', 1000, 'R51', 'R71');
+insert into aerovia values ('AV4', 1000, 'R22', 'R34');
+insert into aerovia values ('AV5', 1000, 'R34', 'R51');
+
+
+-- GRU -> SDU
+insert into rota values (1)
+
+    insert into rota_aerovias (1, 'AV1')
+insert into rota_aerovias (1, 'AV2')
+insert into rota_aerovias (1, 'AV3')
+
+    -- GRU -> SDU
+insert into rota values (2)
+
+
+insert into rota_aerovias (2, 'AV4')
+insert into rota_aerovias (2, 'AV5')
+insert into rota_aerovias (2, 'AV3')
+
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
+insert into aerovia values ('AV1', 1000, 'C1', 'C22');
