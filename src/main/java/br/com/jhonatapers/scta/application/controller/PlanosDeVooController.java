94dfe9ef60cd239fec579832e3885d04bda21c23
leonardo.berlatto <lberlatto@tempo.io>
2022-10-16 18:51:11 -0300

    Começando validação de plano de voo

---
+++ src/main/java/br/com/jhonatapers/scta/application/controller/PlanosDeVooController.java
@@ -0,0 +1,27 @@
+package br.com.jhonatapers.scta.application.controller;
+
+import br.com.jhonatapers.scta.domain.dto.ValidacaoPlanoDeVooDto;
+import br.com.jhonatapers.scta.domain.entity.PlanoDeVoo;
+import br.com.jhonatapers.scta.domain.service.PlanoDeVooService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/planos-de-voo")
+public class PlanosDeVooController {
+
+    private final PlanoDeVooService planoDeVooService;
+
+    @Autowired
+    public PlanosDeVooController(PlanoDeVooService planoDeVooService) {
+        this.planoDeVooService = planoDeVooService;
+    }
+
+    @PostMapping
+    public ValidacaoPlanoDeVooDto verificar(@RequestBody PlanoDeVoo planoDeVoo) {
+        return planoDeVooService.verificar(planoDeVoo);
+    }
+}
---
+++ src/main/java/br/com/jhonatapers/scta/domain/dto/ValidacaoPlanoDeVooDto.java
@@ -0,0 +1,29 @@
+package br.com.jhonatapers.scta.domain.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidacaoPlanoDeVooDto {
+
+    private boolean isValid;
+    private List<String> problemas;
+
+    public ValidacaoPlanoDeVooDto() {
+        this.isValid = true;
+        this.problemas = new ArrayList<>();
+    }
+
+    public void addProblema(String problema) {
+        isValid = false;
+        problemas.add(problema);
+    }
+
+    public void setProblemas(List<String> problemas) {
+        isValid = problemas.isEmpty();
+        this.problemas = problemas;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+}
